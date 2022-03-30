5985f7e32ffef8d79837069f2b97a4ff7802aa86
peanutfisher <meifajia@outlook.com>
2022-03-30 22:33:54 +0800

    add sound and animation when myplane collided with enemies.

--- myplane.py
+++ myplane.py
@@ -9,10 +9,14 @@ class MyPlane(pygame.sprite.Sprite):
         self.width, self.height = bg_size[0], bg_size[1]
         self.image1 = pygame.image.load(image1).convert_alpha()
         self.image2 = pygame.image.load(image2).convert_alpha()
+        self.destroy_images = [pygame.image.load('images\me_destroy_1.png').convert_alpha(), pygame.image.load('images\me_destroy_2.png').convert_alpha(),\
+                               pygame.image.load('images\me_destroy_3.png').convert_alpha(), pygame.image.load('images\me_destroy_4.png').convert_alpha()]
         self.rect = self.image1.get_rect()
         self.rect.left, self.rect.top = (self.width - self.rect.width) // 2, \
                             self.height - self.rect.height - 60
         self.speed = 10
+        self.active = True
+        self.mask = pygame.mask.from_surface(self.image1)
 
     def moveUp(self):
             if self.rect.top > 0:
